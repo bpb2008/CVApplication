8a5b07b474d042c982fd180d7553a1495fe9ae47
bpb2008 <brittanythompson08@gmail.com>
2023-11-21 11:53:19 -0600

    added .gitignore

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Created by bpb2008</p>
+      <a href="https://github.com/bpb2008">Find more cool creations here!</a>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1>CV Generator</h1>
+    </div>
+  );
+};
+
+export default Header;
